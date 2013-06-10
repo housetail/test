41e8d91b4875611333788b097c760ce8d5333b4c
housetail <chensw03@gmail.com>
2013-06-10 17:08:16 +0800

    add test case bitset

---
+++ cpp/test_bitset.cpp
@@ -0,0 +1,14 @@
+#include <bitset>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+   bitset<32> bs1;
+   bitset<32> bs2;
+   bitset<32> bs3;
+
+   bs1 = bs2 ^ bs3;
+
+   return 0;
+}
